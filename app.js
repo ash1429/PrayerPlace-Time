4dacc35a3c2b3c1ab577f6670fefcd2b88efd125
Aashiq <md.ashikuzzaman.1429@gmail.com>
2019-03-20 11:16:53 +0600

    edited port address

--- app.js
+++ app.js
@@ -74,6 +74,6 @@ app.use('/signup', auth.notUser, signup);
 app.use('/login', auth.notUser, login);
 app.use('/logout', auth.user, logout);
 
-app.listen(3000, () => {
-  console.log("Server is running");
+app.listen(process.env.PORT, process.env.IP, function () {
+  console.log("Server Has Started!");
 });
