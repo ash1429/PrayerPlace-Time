a23bba2f1b03db3c01901bfb7b0bdfca8cdd37e5
Aashiq <md.ashikuzzaman.1429@gmail.com>
2019-03-20 13:17:04 +0600

    fixed index.ejs a little bit

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use('/signup', auth.notUser, signup);
 app.use('/login', auth.notUser, login);
 app.use('/logout', auth.user, logout);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log("Server Has Started!");
 });
